d62a0cf0e55004ec0be0e0daf83738e2ad69898a
senselesslol <tuananhdang010707@gmail.com>
2025-04-25 13:22:46 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-        var repoName = "change in config.js"; 
-        var repoDescription = "open config.js with your text editor of choice."; 
-
-        document.querySelector('.repoName').innerText = repoName;
-        document.querySelector('.repodesc').innerText = repoDescription;
